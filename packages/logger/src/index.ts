947ee5ffc816aad173d5e4c3b1c0d8bd2785a48e
John Wright <jrite@protonmail.com>
2021-06-07 14:46:57 +0100

    fix(logger): export file logger behavior

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -4,4 +4,5 @@ export default Logger
 
 export { default as ConsoleLoggerBehavior } from './ConsoleLoggerBehavior'
 export { default as DisabledLoggerBehavior } from './DisabledLoggerBehavior'
+export { default as FileLoggerBehavior } from './FileLoggerBehavior'
 export { default as LoggerBehavior } from './LoggerBehavior'
