417938ab819a446629d096aca63fc00638672e7c
John Wright <jrite@protonmail.com>
2021-06-07 14:13:49 +0100

    feat(logger): multiple behaviours

--- packages/logger/src/Logger.ts
+++ packages/logger/src/Logger.ts
@@ -1,37 +1,49 @@
 import LoggerBehavior from './LoggerBehavior'
 
 export default class Logger implements LoggerBehavior {
+  readonly #behaviors: LoggerBehavior[]
+  readonly #label: string
+  readonly #name: string
+
   constructor(
-    protected name: string,
-    private behavior: LoggerBehavior,
-    private label: string = `[${name}]`
-  ) {}
+    name: string,
+    behaviors: LoggerBehavior | LoggerBehavior[],
+    label: string = `[${name}]`
+  ) {
+    this.#behaviors = Array.isArray(behaviors) ? behaviors : [behaviors]
+    this.#label = label
+    this.#name = name
+  }
+
+  get name() {
+    return this.#name
+  }
 
   debug(...args: any[]) {
-    this.behavior.debug(this.label, ...args)
+    this.#behaviors.forEach((b) => b.debug(this.#label, ...args))
   }
 
   error(...args: any[]) {
-    this.behavior.error(this.label, ...args)
+    this.#behaviors.forEach((b) => b.error(this.#label, ...args))
   }
 
   extend(label: string) {
-    return new Logger(this.name, this.behavior, `${this.label}[${label}]`)
+    return new Logger(this.#name, this.#behaviors, `${this.#label}[${label}]`)
   }
 
   info(...args: any[]) {
-    this.behavior.info(this.label, ...args)
+    this.#behaviors.forEach((b) => b.info(this.#label, ...args))
   }
 
   log(...args: any[]) {
-    this.behavior.log(this.label, ...args)
+    this.#behaviors.forEach((b) => b.log(this.#label, ...args))
   }
 
   table(...args: any[]) {
-    this.behavior.table(this.label, ...args)
+    this.#behaviors.forEach((b) => b.table(this.#label, ...args))
   }
 
   warn(...args: any[]) {
-    this.behavior.warn(this.label, ...args)
+    this.#behaviors.forEach((b) => b.warn(this.#label, ...args))
   }
 }
