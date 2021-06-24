368198205dd83357a61fe6528a1f269a12adac41
John Wright <jrite@protonmail.com>
2021-06-24 15:54:59 +0100

    feat(store-logger): add store logging label

--- packages/store-logger/src/index.ts
+++ packages/store-logger/src/index.ts
@@ -1,12 +1,18 @@
 import type { Store } from '@plugola/store'
 import type { Logger } from '@plugola/logger'
 
-export default function storeLogger(store: Store<any, any>, logger: Logger) {
+export default function storeLogger(
+  store: Store<any, any>,
+  logger: Logger,
+  label = 'store'
+) {
+  const storeLogger = logger.extend(label)
+
   store.subscribe((action, state) => {
-    logger.info(action, state)
+    storeLogger.info(action, state)
   })
 
   store.subscribeToStaleEvents((action) => {
-    logger.info(action, 'NO_CHANGE')
+    storeLogger.info(action, 'NO_CHANGE')
   })
 }
